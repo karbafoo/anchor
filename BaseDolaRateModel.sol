4d67dd1b8c09378eb2420d98e2434b4cf295f791
karbafoo <karbafooo@gmail.com>
2022-03-11 20:37:30 +0330

    created BaseDolaRateModel

--- BaseDolaRateModel.sol
+++ BaseDolaRateModel.sol
@@ -2,6 +2,23 @@ pragma solidity ^0.5.16;
 
 import "./SafeMath.sol";
 
+interface IFeed {
+    function decimals() external view returns (uint8);
+    function latestAnswer() external view returns (uint);
+}
+
+
+contract DolaFeed is IFeed {
+
+    function decimals() public view returns(uint8) {
+        return 18;
+    }
+
+    function latestAnswer() public view returns (uint) {
+        return 1 ether;
+    }
+
+}
 /**
   * @title Logic for Compound's JumpRateModel Contract V2.
   * @author Compound (modified by Dharma Labs, refactored by Arr00)
@@ -43,6 +60,44 @@ contract BaseJumpRateModelV2 {
     uint public kink;
 
     /**
+     * @notice  The depeg threshold. [0] = min, [1] = max
+     */
+    DolaFeed dolaFeed;
+
+    /**
+     * @notice  The depeg threshold. [0] = min, [1] = max
+     */
+    uint[2] depegThreshold = [0,0];
+
+    /**
+     * @notice The range of borrow rate. [0] = min, [1] = max
+     */
+    uint[2] rateRange = [0,0];
+
+    /**
+     * @notice The based borrow rate
+     */
+    uint baseRate; 
+    
+    /**
+     * @notice The current borrow rate
+     */
+    uint currentRate;
+
+    /**
+     * @notice The borrow rate step amount
+     */
+    uint rateStep;
+
+    /**
+     * @notice The minimum time between each update
+     */
+    uint updateRateInterval;
+    /**
+     * @notice The timestmap for the latest update
+     */
+    uint lastUpdateTimestamp;
+    /**
      * @notice Construct an interest rate model
      * @param baseRatePerYear The approximate target base APR, as a mantissa (scaled by 1e18)
      * @param multiplierPerYear The rate of increase in interest rate wrt utilization (scaled by 1e18)
@@ -50,9 +105,12 @@ contract BaseJumpRateModelV2 {
      * @param kink_ The utilization point at which the jump multiplier is applied
      * @param owner_ The address of the owner, i.e. the Timelock contract (which has the ability to update parameters directly)
      */
-    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_, address owner_) internal {
+    constructor(uint baseRatePerYear, uint multiplierPerYear, uint jumpMultiplierPerYear, uint kink_,
+                 uint baseRate, uint currentRate, uint rateStep, uint[2] memory depegThreshold, uint[2] memory rateRange, uint updateRateInterval,
+                 address dolaFeed_, address owner_) internal {
         owner = owner_;
-
+        dolaFeed = DolaFeed(dolaFeed_);
+        updateDolaParams(baseRate, currentRate, rateStep, depegThreshold, rateRange, updateRateInterval);
         updateJumpRateModelInternal(baseRatePerYear,  multiplierPerYear, jumpMultiplierPerYear, kink_);
     }
 
@@ -131,7 +189,58 @@ contract BaseJumpRateModelV2 {
         multiplierPerBlock = (multiplierPerYear.mul(1e18)).div(blocksPerYear.mul(kink_));
         jumpMultiplierPerBlock = jumpMultiplierPerYear.div(blocksPerYear);
         kink = kink_;
+    }
+    /**
+     * @notice Internal function to update the parameters of the dola interest rate model
+     * @param baseRate
+     * @param currentRate
+     * @param rateStep 
+     * @param depegThreshold 
+     * @param rateRange 
+     * @param updateRateInterval 
+     */
+    function updateDolaParams(uint baseRate, uint currentRate, uint rateStep, uint[2] memory depegThreshold, uint[2] memory rateRange, uint updateRateInterval) internal {
+        require(msg.sender == owner, "only the owner may call this function.");
+        baseRate = baseRate;
+        currentRate = currentRate;
+        rateStep = rateStep;
+        depegThreshold = depegThreshold;
+        rateRange = rateRange;
+        updateRateInterval = updateRateInterval;
+    }
+        
+ 
+    function updateRate() public returns (bool) {
+        if(lastUpdateTimestamp + updateRateInterval < block.timestamp){
+            lastUpdateTimestamp = block.timestamp;
 
-        emit NewInterestParams(baseRatePerBlock, multiplierPerBlock, jumpMultiplierPerBlock, kink);
+            uint dolaPrice = dolaFeed.latestAnswer();
+
+            if(dolaPrice < depegThreshold[0]){
+                currentRate -= rateStep;
+            }
+            else if (dolaPrice > depegThreshold[1]){
+                currentRate += rateStep;
+            }
+            else{
+                // If DOLA price is between the two thresholds AND borrow rate is
+                //  not equal to baseRate, decrease or increase the borrow rate to 
+                //  the direction of baseRate by rateStep e.g. if the borrow rate is 
+                //  higher than baseRate, the borrow rate would decrease by rateStep 
+                //  on each function call until it reaches baseRate.
+                if(currentRate + rateStep < baseRate){
+                    currentRate += rateStep;
+                }
+                else if(currentRate - rateStep > baseRate){
+                    currentRate -= rateStep;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    function getDolaRate() returns (uint){
+        return currentRate;
     }
 }
